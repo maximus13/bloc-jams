b6aa138e61005f7dd17c88ee9575a54ee5d04cb0
Maximus13 <tlembi@intellirentsf.com>
2015-03-23 13:38:54 -0700

    fix collection view issue

--- app/scripts/collection.js
+++ app/scripts/collection.js
@@ -1,20 +1,20 @@
- var buildAlbumThumbnail = function() {
-  var template =
-  <div class="collection-album-container col-md-2">
-  <img src="/images/album-placeholder.png"/>
-  <div class="caption album-collection-info">
-  <p>
-  <a class="album-name" href="/album.html"> Album Name </a>
-  <br/>
-  <a href="/album.html"> Artist name </a>
-  <br/>
-  X songs
-  <br/>
-  X:XX Total Length
-  <br/>
-  </p>
-  </div>
-  </div>;
+var buildAlbumThumbnail = function() {
+   var template =
+       '<div class="collection-album-container col-md-2">'
+     + '  <img src="/images/album-placeholder.png"/>'
+     + '  <div class="caption album-collection-info">'
+     + '    <p>'
+     + '      <a class="album-name" href="/album.html"> Album Name </a>'
+     + '      <br/>'
+     + '      <a href="/album.html"> Artist name </a>'
+     + '      <br/>'
+     + '      X songs'
+     + '      <br/>'
+     + '   X:XX Total Length'
+     + '      <br/>'
+     + '    </p>'
+     + '  </div>'
+     + '</div>';
 
   return $(template);
 };
